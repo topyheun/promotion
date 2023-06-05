2fbf6dfe49176740eb98bec86b3cd89bd48187ac
Topy <gmsdl1994@naver.com>
2023-06-05 16:32:36 +0900

    create project

---
+++ src/main/java/topy/promotion/PromotionApplication.java
@@ -0,0 +1,13 @@
+package topy.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromotionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/topy/promotion/PromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package topy.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
