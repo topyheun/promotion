7bf0fb01276de59cfe8dab18be2ad2933a19761a
Topy <gmsdl1994@naver.com>
2023-06-08 16:57:45 +0900

    feat: Add auditing setting

--- src/main/java/topy/promotion/PromotionApplication.java
+++ src/main/java/topy/promotion/PromotionApplication.java
@@ -2,8 +2,10 @@ package topy.promotion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PromotionApplication {
 
     public static void main(String[] args) {
