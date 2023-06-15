050493a9bfff8edf2885fa41c495c32bc58a49b3
Topy <gmsdl1994@naver.com>
2023-06-15 18:04:08 +0900

    feat: Add Winner repository

---
+++ src/main/java/topy/promotion/modules/promotion/WinnerRepository.java
@@ -0,0 +1,10 @@
+package topy.promotion.modules.promotion;
+
+import lombok.extern.java.Log;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import topy.promotion.modules.promotion.domain.Winner;
+
+@Repository
+public interface WinnerRepository extends JpaRepository<Winner, Log> {
+}
