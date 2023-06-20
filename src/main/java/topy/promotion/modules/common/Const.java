8817087c11651204f6b9bd6826a3c1052089509d
Topy <gmsdl1994@naver.com>
2023-06-20 14:38:31 +0900

    feat: Rename const

--- src/main/java/topy/promotion/modules/common/Const.java
+++ src/main/java/topy/promotion/modules/common/Const.java
@@ -9,7 +9,7 @@ public class Const {
     public static final String USER_DTO_NO_USERNAME = "아이디를 입력해주세요";
     public static final String USER_DTO_NO_PASSWORD = "비밀번호를 입력해주세요";
     public static final String USER_DTO_NO_EMAIL = "이메일을 입력해주세요";
-    public static final String USER_DTO_WRONG_USERNAME_FORMAT = "올바르지 않은 아이디 양식입니다";
+    public static final String USER_DTO_WRONG_USERNAME_FORMAT = "올바르지 않은 아이디 양식입니다 (영문 소문자와 숫자를 사용해서 생성해주세요 글자 수 제한은 4 ~ 10자리 입니다)";
     public static final String USER_DTO_WRONG_PASSWORD_FORMAT = "올바르지 않은 비밀번호 양식입니다";
     public static final String USER_DTO_WRONG_EMAIL_FORMAT = "올바르지 않은 이메일 양식입니다";
 
