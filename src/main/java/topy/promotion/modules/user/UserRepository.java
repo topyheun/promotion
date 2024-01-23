da26924c466c551c74ca1302947df49fc789b60d
Topy <gmsdl1994@naver.com>
2024-01-23 13:15:10 +0900

    user repository 안티 패턴 제거

--- src/main/java/topy/promotion/modules/user/UserRepository.java
+++ src/main/java/topy/promotion/modules/user/UserRepository.java
@@ -2,10 +2,8 @@ package topy.promotion.modules.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUsername(String username);
