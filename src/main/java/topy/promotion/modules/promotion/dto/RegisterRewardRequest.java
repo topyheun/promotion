cc2f220e3af9377d4f8111ba8926332ff69bd926
Topy <gmsdl1994@naver.com>
2024-01-23 13:23:58 +0900

    request dto 필요없는 어노테이션 제거

--- src/main/java/topy/promotion/modules/promotion/dto/RegisterRewardRequest.java
+++ src/main/java/topy/promotion/modules/promotion/dto/RegisterRewardRequest.java
@@ -8,16 +8,12 @@ import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 import topy.promotion.modules.promotion.domain.Promotion;
 import topy.promotion.modules.promotion.domain.Rank;
 import topy.promotion.modules.promotion.domain.Reward;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
 public class RegisterRewardRequest {
 
     @NotBlank(message = REWARD_DTO_NO_VALUES)
