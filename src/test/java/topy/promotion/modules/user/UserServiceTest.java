ed9cdc3cffa98e017bf545a7520d37e2e913b1ec
Topy <gmsdl1994@naver.com>
2024-01-23 13:26:25 +0900

    test-signup 기능 메서드명 수정

--- src/test/java/topy/promotion/modules/user/UserServiceTest.java
+++ src/test/java/topy/promotion/modules/user/UserServiceTest.java
@@ -9,10 +9,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.ActiveProfiles;
 import topy.promotion.modules.user.dto.UserSignUpRequest;
 
-@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
@@ -24,7 +22,7 @@ class UserServiceTest {
 
     @Test
     @DisplayName("회원가입 시 사용자가 이미 존재한다면 Exception 발생한다.")
-    void should_Exception_when_UserExistsWhenSignUp() {
+    void should_ThrowException_when_UserExistsWhenSignUp() {
         // Arrange
         UserSignUpRequest userSignUpRequest = new UserSignUpRequest("username", "password", "email@example.com");
         when(userRepository.existsByUsername(userSignUpRequest.getUsername())).thenReturn(true);
