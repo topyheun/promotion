952bdf1446f043e9c3620fd08b70aefc00f2ee3d
Topy <gmsdl1994@naver.com>
2024-02-09 17:16:48 +0900

    test-참여 생성

---
+++ src/test/java/topy/promotion/modules/promotion/service/ParticipationCreateServiceTest.java
@@ -0,0 +1,40 @@
+package topy.promotion.modules.promotion.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import topy.promotion.modules.promotion.domain.Participation;
+import topy.promotion.modules.promotion.domain.Promotion;
+import topy.promotion.modules.promotion.repository.ParticipationRepository;
+import topy.promotion.modules.user.User;
+
+@ExtendWith(MockitoExtension.class)
+class ParticipationCreateServiceTest {
+
+    @Mock
+    private ParticipationRepository participationRepository;
+
+    @InjectMocks
+    private ParticipationCreateService sut;
+
+    @Test
+    @DisplayName("사용자와 프로모션이 제공될 때 참여 정보가 저장되어야 한다.")
+    void should_SaveParticipation_When_UserAndPromotionAreProvided() {
+        // Arrange
+        User mockUser = mock(User.class);
+        Promotion mockPromotion = mock(Promotion.class);
+
+        // Act
+        sut.saveParticipation(mockUser, mockPromotion);
+
+        // Assert
+        verify(participationRepository).save(any(Participation.class));
+    }
+}
